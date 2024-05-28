df4a53abe55012846068700699b5312881594747
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-05-28 15:52:04 +1000

    Improved documentation on what each class will do

---
+++ christmas_crackers.py
@@ -0,0 +1,4 @@
+"""" Who ever gets the party hat from the christmas cracker wins
+- House pays out 1.95x when player wins
+- House takes all if player player loses
+"""
--- higher_lower.py
+++ higher_lower.py
@@ -1,1 +1,8 @@
-""" Randomly picks a number between 0 - 100 where """
+""" Randomly picks a number between 0 - 100
+Player has two options:
+- High >= 55
+- Low  <= 50
+
+If the number lands in between 51-54, player will lose.
+If the player calls the correct option, they will receive 2x their stake
+"""
