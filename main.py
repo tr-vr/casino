d97d8699646011f66520a5ef2e7e5ca43fd0b354
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-05-28 19:43:30 +1000

    changed welcome message

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 """ Main class that will be used to operate the casino """
 import time
+import sys
 
 def welcome(): #Introduction to the games available
     print("Welcome to Fortune Oasis")
     time.sleep(3)
-    print("At Fortune Oasis, we currently have Christmas Crackers & Dicing available.")
-    time.sleep(3)
+    print("At Fortune Oasis, we currently have two games available.")
+    time.sleep(1)
+    print("Christmas Cracker [1.95x] & Dicing [2x]")
+    time.sleep(2)
     print("If you want to learn how to play, type \'!rules\' in the chat below.")
-    time.sleep(3)
-    print()
+    time.sleep(2)
+    print("~" * 65)
     print("Select your game options with these commands.")
     print("Christmas Crackers: !You / !Me - OR - Dicing: !H / !L")
-
-def rules():
-    
 
 def main():
     welcome()
