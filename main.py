f7fd0a1c32cdde74701f18f12eca0ed3889ac027
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-05-28 16:00:56 +1000

    added welcome + main func

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 """ Main class that will be used to operate the casino """
 
+def welcome():
+    print("Welcome to Fortune Oasis Casino! ")
+
+def main():
+    welcome()
+
+if __name__ == "__main__":
+    main()
