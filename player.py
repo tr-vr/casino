b256f848143651379af9bb57aba1ca0e17e0404e
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-05-28 16:45:11 +1000

    added player class Hoping to learn how to store information.

---
+++ player.py
@@ -0,0 +1,8 @@
+""" This will contain the players information
+Username:
+Current Wallet Amount:
+Total Wagered:
+
+Will want to learn how to store this information, so that when a player quits how they will
+resume the game will the stats from last time. 
+"""
