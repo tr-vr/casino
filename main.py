20f5771d26c7f8e129fa1ea4bd5fd94038847183
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-05-28 16:04:37 +1000

    import time

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 """ Main class that will be used to operate the casino """
+import time
 
 def welcome():
-    print("Welcome to Fortune Oasis Casino! ")
+    print("Welcome to Fortune Oasis Casino!")
+    time.sleep(3)
+    print("")
+
 
 def main():
     welcome()
