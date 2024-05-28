7f0c5731aa91b2f65e060b12a975c8af3aa607d3
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-05-28 16:28:52 +1000

    welcome() introduction added

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 """ Main class that will be used to operate the casino """
 import time
 
-def welcome():
+def welcome(): #Introduction to the games available
     print("Welcome to Fortune Oasis Casino!")
     time.sleep(3)
-    print("")
-
+    print("At Fortune Oasis, we currently have Christmas Crackers & Dicing available.")
+    time.sleep(3)
+    print("If you want to learn how to play, type \'!rules\' in the chat below.")
+    time.sleep(3)
+    print()
+    print("Select your game options with these commands.")
+    print("Christmas Crackers: !You / !Me - OR - Dicing: !H / !L")
 
 def main():
     welcome()
