72990dde03f61345ab8658ef6b67137a49df3781
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-05-29 16:06:52 +1000

    Setup sqlite3 + playerstats database

--- Fortune Oasis/player.py
+++ Fortune Oasis/player.py
@@ -6,3 +6,13 @@ Total Wagered:
 Will want to learn how to store this information, so that when a player quits how they will
 resume the game will the stats from last time. 
 """
+
+import sqlite3
+
+con = sqlite3.connect('playerinfo.db')
+cur = con.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS playerstats
+                (user text, defaultgame text, balance real, wagered real, 
+                crackerWins real, crackerLoss real, crackerPnL real,
+                DiceWin real, DiceLoss real, DicePnL real, TotalPnL real)''')
