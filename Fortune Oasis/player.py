2c8e8ec6acb654ba3163c8e41f3616835038595a
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-05-29 16:43:41 +1000

    Fix text wrapping

--- Fortune Oasis/player.py
+++ Fortune Oasis/player.py
@@ -3,7 +3,8 @@ Username:
 Current Wallet Amount:
 Total Wagered:
 
-Will want to learn how to store this information, so that when a player quits how they will
+Will want to learn how to store this information, so that when a player quits
+how they will
 resume the game will the stats from last time. 
 """
 
