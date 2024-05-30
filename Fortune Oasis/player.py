3299b07ae4249c5fb4490c35e186db1404ebb601
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-05-30 23:33:56 +1000

    Add connect sql into a function

--- Fortune Oasis/player.py
+++ Fortune Oasis/player.py
@@ -8,10 +8,15 @@
 
 import sqlite3
 
-con = sqlite3.connect('playerinfo.db')
-cur = con.cursor()
+
 
 cur.execute('''CREATE TABLE IF NOT EXISTS playerstats
                 (user text PRIMARY KEY, defaultgame text, balance real, wagered
                  real, crackerWins real, crackerLoss real, crackerPnL real,
                 DiceWin real, DiceLoss real, DicePnL real, TotalPnL real)''')
+
+# Create a connection to the data base
+def connect():
+    con = sqlite3.connect('playerinfo.db')
+    cur = con.cursor()
+    return con, cur
