5559d1f045a6535fadcf810ff96d85cd5708e734
tr-vr <140265392+tr-vr@users.noreply.github.com>
2024-05-30 23:54:34 +1000

    Change to block comments

--- Fortune Oasis/christmas_crackers.py
+++ Fortune Oasis/christmas_crackers.py
@@ -1,11 +1,10 @@
-"""" Who ever gets the party hat from the christmas cracker wins
-- House pays out 1.95x when player wins
-- House takes all if player player loses
-
-You pull a christmas cracker
-The person you pulled the cracker with gets the prize.
-You get the prize from the cracker.
-"""
+# Who ever gets the party hat from the christmas cracker wins
+# - House pays out 1.95x when player wins
+# - House takes all if player player loses
+# Dialogue:
+# You pull a christmas cracker
+# The person you pulled the cracker with gets the prize.
+# You get the prize from the cracker.
 
 def name():
     return "!You"
